223e9525a15d82816d5f48f4b00efbf5f603d948
Johannes Raggam <raggam-nl@adm.at>
2013-12-03 12:32:49 +0100

    formating, pep8

--- setup.py
+++ setup.py
@@ -1,38 +1,36 @@
 from setuptools import setup, find_packages
 import os
 
-<<<<<<< HEAD
-version = '0.5.dev0'
-=======
 version = '2.0.dev0'
->>>>>>> Add CHANGES note, bump version, require p.a.u 2.0
+long_description = open("README.rst").read() + "\n"\
+    + open(os.path.join("docs", "HISTORY.txt")).read() + "\n"\
+    + open(os.path.join("docs", "TODO.txt")).read(),
 
-setup(name='collective.examples.userdata',
-      version=version,
-      description="Showcase for the new (Plone 4) plone.app.users IUserDataSchema. Shows how to extend the user data fields that can be selected for the registration form.",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read() + "\n"
-                       + open(os.path.join("docs", "TODO.txt")).read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='collective.examples.userdata',
+    version=version,
+    description="Showcase for the new (Plone 4) plone.app.users "
+                "IUserDataSchema. Shows how to extend the user data fields "
+                "that can be selected for the registration form.",
+    long_description=long_description,
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Kees Hink',
-      author_email='',
-      url='https://github.com/collective/collective.examples.userdata',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective', 'collective.examples'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.users >= 2.0',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={
+    ],
+    keywords='',
+    author='Kees Hink',
+    author_email='',
+    url='https://github.com/collective/collective.examples.userdata',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective', 'collective.examples'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.app.users >= 2.0',
+    ],
+    extras_require={
         'test': ['plone.app.testing'],
-      },
-      )
+    },
+)
