674eee30b5b907cd58d2ff1edfe7711529966866
Jamie Lentin <jm@lentin.co.uk>
2014-07-03 20:42:57 +0100

    Preparing release 2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0.dev0'
+version = '2.0'
 long_description = open("README.rst").read() + "\n"\
     + open(os.path.join("docs", "HISTORY.txt")).read() + "\n"\
     + open(os.path.join("docs", "TODO.txt")).read(),
