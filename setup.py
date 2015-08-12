3b2d6ccdc70db89bfb69669ffc3ab857cff50b6f
Martin Peeters <martin.peeters@affinitic.be>
2015-08-12 14:02:38 +0200

    Update package classifiers

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     long_description=long_description,
     classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
     ],
     keywords='',
